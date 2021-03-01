4bebaa5b20ae63b9674e0bf03c2e9239143b25ab
sidd2001 <72477036+sidd2001@users.noreply.github.com>
2021-02-28 18:24:18 -0700

    Update GitDemo.java
    Edit name to Siddharth Agarwal 
    
    added variable to class GitDemo (  private double reverseNum; )
    
    edited addNumber function ( previousNumber = previousNumber + num;
                                                        return previousNumber;  )
    
    edited reverse number function (  reverseNum = newNumber; )

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+Siddharth Agarwal
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -14,7 +14,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber = previousNumber + num;
+        return previousNumber;
     }
     
     /*****
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
